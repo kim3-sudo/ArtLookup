39a0fbf6fb5f51ce9a71adea56ef1d42bc893682
Sejin Kim <kim3@kenyon.edu>
2019-11-10 14:40:52 -0500

    Create pseudo_topLiked.cpp

---
+++ pseudo/pseudo_topLiked.cpp
@@ -0,0 +1,13 @@
+// DO NOT TRY TO COMPILE THIS CODE! IT WON'T WORK!
+
+/* ####################### */
+/* top liked images lookup */
+/* ####################### */
+
+/*
+SELECT * FROM database /* looking at the likes column */ DESC */limited to 10 items*/;
+Push that to an array
+Return the array
+*/
+
+//fill me with code :)
