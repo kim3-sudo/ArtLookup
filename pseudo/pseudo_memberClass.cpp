dec82e4a6ff5066dffe0526304a831eaef7b8068
Sejin Kim <kim3@kenyon.edu>
2019-11-10 15:00:11 -0500

    Update pseudo_memberClass.cpp

--- pseudo/pseudo_memberClass.cpp
+++ pseudo/pseudo_memberClass.cpp
@@ -1,1 +1,7 @@
+// DO NOT TRY TO COMPILE THIS CODE! IT WON'T WORK!
 
+/* ################################## */
+/* append a table with likes/comments */
+/* ################################## */
+
+// Purpose: to like and comment on images
