85daecab7968df1446683e36d11d9c43a466c266
Ryan Schultz <ryanschultz2018@yahoo.com>
2019-11-11 17:04:26 -0500

    Created header file for Artwork class

---
+++ artlookup/Artwork.h
@@ -0,0 +1,49 @@
+#using <iostream>
+#using <string>
+
+using namespace std;
+
+
+#ifndef ARTWORK_H
+#define ARTWORK_H
+
+Class ArtLookup
+{
+  public:
+    ArtLookup();
+    ArtLookup(int artId, string title, string url, int numLikes, string author, string birthDeath, int date, string location, string timeframe, string technique, string school, string type, string form);
+
+    int artId;
+    int numLikes;
+    string title;
+    string url;
+
+    struct ArtistInfo
+    {
+      string author;
+      string birthDeath;
+    };
+    struct ArtSetting
+    {
+      int date;
+      string location;
+      string timeframe;
+    };
+    struct ArtStyle
+    {
+      string technique;
+      string school;
+      string type;
+      string form;
+    };
+
+
+    //Mutator
+    void incrementLikes();
+
+  private:
+
+
+};
+
+#endif /* ARTWORK_H */
