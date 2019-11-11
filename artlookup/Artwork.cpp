8ef0f190901a3d96428376ef956fca9e33797981
Ryan Schultz <ryanschultz2018@yahoo.com>
2019-11-11 17:07:18 -0500

    Adding artlookup folder to store code

---
+++ artlookup/Artwork.cpp
@@ -0,0 +1,13 @@
+
+
+
+
+
+#include "mysql_connection.h"
+#include "mysql_driver.h"
+#include <cppconn/driver.h>
+#include <cppconn/exception.h>
+#include <cppconn/resultset.h>
+#include <cppconn/statement.h>
+#include <cppconn/prepared_statement.h>
+#include "ArtLookup.h"
