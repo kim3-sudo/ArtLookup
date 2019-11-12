d2499ef2517462636ba9449592e17ad33e5c41f3
Ryan Schultz <ryanschultz2018@yahoo.com>
2019-11-12 15:53:29 -0500

    ArtDBCommunicator header file

---
+++ artlookup/ArtDBCommunicator.h
@@ -0,0 +1,21 @@
+#using <string>
+
+using namespace std;
+
+
+#ifndef ARTDBCOMMUNICATOR_H
+#define ARTDBCOMMUNICATOR_H
+
+class ArtDBCommunicator{
+  public:
+
+  private:
+    const string password;
+    const string DBname;
+    const string address;
+    const string DBUsername;
+
+
+};
+
+#endif ARTDBCOMMUNICATOR_H
