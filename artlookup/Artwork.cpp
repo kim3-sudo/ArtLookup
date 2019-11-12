25c9d1c5de40cde376174e3ea541eb653a69c961
Michaela Brydon <brydon1@kenyon.edu>
2019-11-12 16:39:15 -0500

    Moved structs outside of class ArtLookup in Artwork.h and edited constructor in Artwork.cpp

--- artlookup/Artwork.cpp
+++ artlookup/Artwork.cpp
@@ -11,16 +11,26 @@ Artwork::Artwork()
   Postconditions: Relevant data is stored in member variables including ArtistInfo, ArtSetting and ArtStyle structs
 */
 //This is very long MICHAELA!!! (Not mad just need your help)
-Artwork::Artwork(int artIdFromDB, string titleFromDB, string urlFromDB, int numLikesFromDB, string authorFromDB, string birthDeathFromDB, int dateFromDB, string locationFromDB, string timeframeFromDB, string techniqueFromDB, string schoolFromDB, string typeFromDB, string formFromDB)
+Artwork::Artwork(int artIdFromDB, string titleFromDB, string urlFromDB, 
+  int numLikesFromDB, string authorFromDB, string birthDeathFromDB, int dateFromDB, 
+  string locationFromDB, string timeframeFromDB, string techniqueFromDB, string schoolFromDB, string typeFromDB, string formFromDB)
 {
   artId = artIdFromDB;  //Alternate solution this->artId = artId
   title = titleFromDB;
   url = urlFromDB;
   numLikes = numLikesFromDB;
 
-  ArtistInfo artistInfo{ authorFromDB, birthDeathFromDB };
-  ArtSetting setting{ dateFromDB, locationFromDB, timeframeFromDB };
-  ArtStyle style{ techniqueFromDB, schoolFromDB, typeFromDB, formFromDB };
+  artistInfo.author = authorFromDB;
+  artistInfo.birthDeath = birthDeathFromDB;
+
+  artSetting.date = dateFromDB;
+  artSetting.location = locationFromDB;
+  artSetting.timeframe = timeframeFromDB;
+
+  artStyle.technique = techniqueFromDB;
+  artStyle.school = schoolFromDB;
+  artStyle.type = typeFromDB;
+  artStyle.form = formFromDB;
 }
 
 /*Preconditions: None
--- artlookup/Artwork.h
+++ artlookup/Artwork.h
@@ -15,43 +15,43 @@ using namespace std;
 #ifndef ARTWORK_H
 #define ARTWORK_H
 
-Class ArtLookup
-{
+struct ArtistInfo {
+  string author;
+  string birthDeath;
+
+  };
+
+struct ArtSetting {
+    int date;
+    string location;
+    string timeframe;
+  };
+
+struct ArtStyle {
+    string technique;
+    string school;
+    string type;
+    string form;
+  };
+
+Class ArtLookup {
   public:
     ArtLookup();
-    ArtLookup(int artId, string title, string url, int numLikes, string author, string birthDeath, int date, string location, string timeframe, string technique, string school, string type, string form);
-
-    int artId;
-    int numLikes;
-    string title;
-    string url;
-
-    struct ArtistInfo
-    {
-      string author;
-      string birthDeath;
-    };
-    struct ArtSetting
-    {
-      int date;
-      string location;
-      string timeframe;
-    };
-    struct ArtStyle
-    {
-      string technique;
-      string school;
-      string type;
-      string form;
-    };
-
-
+    ArtLookup(int artId, string title, string url, int numLikes, 
+      string author, string birthDeath, int date, string location, 
+      string timeframe, string technique, string school, string type, 
+      string form);
     //Mutator
     void incrementLikes();
 
   private:
-
-
+    int artId;
+    int numLikes;
+    string title;
+    string url;
+    ArtistInfo artistInfo;
+    ArtSetting artSetting;
+    ArtStyle artStyle;
 };
 
 #endif /* ARTWORK_H */
