f1e93da3ea02d7243294319f701ec4c77067296f
kim3 <kim3@kenyon.edu>
2019-11-12 16:08:35 -0500

    Add some commments to this pseudo code, while discussing memeber functio

--- pseudo/pseudo_ArtLookup.cpp
+++ pseudo/pseudo_ArtLookup.cpp
@@ -6,22 +6,24 @@
 
 // we'll need another string for column name, since this is also the *all* art lookup
 
+// should we have this as a member function? {
 vector<ArtLookup> ArtLookupDB::lookupAll(string search) {
     // code to connect to the database
     sql::Driver* driver = sql::mysql::get_driver_instance(); //loading in drivers
     std::auto_ptr<sql::Connection> con(driver->connect(url, user, pass)); //set up the connector
     con->setSchema(database); //set the schema
-  
+// }
+
     //creates a statement for our command
     std::auto_ptr<sql::Statement> stmt(con->createStatement());
 
-    
+
     vector<Artwork> list;
     // select all from art where the cell value is like the search query
     stmt->execute("SELECT * FROM Art WHERE cell like '%"+search+"%'");
     // This will also need modified for the *all* art lookup
     std::auto_ptr< sql::ResultSet > resultsSet; //get the set of rows
-    
+
     // a loop to pull the data out of the database
     do {
       res.reset(stmt->getResultSet());
