3b15d8b65bf679c9423db62b221fee810c962dc0
Ryan Schultz <ryanschultz2018@yahoo.com>
2019-11-12 16:24:03 -0500

    Added SQL connection headers

--- artlookup/ArtDBCommunicator.h
+++ artlookup/ArtDBCommunicator.h
@@ -1,7 +1,12 @@
-#using <string>
+#include <string>
 
-using namespace std;
-
+#include "mysql_connection.h"
+#include "mysql_driver.h"
+#include <cppconn/driver.h>
+#include <cppconn/exception.h>
+#include <cppconn/resultset.h>
+#include <cppconn/statement.h>
+#include <cppconn/prepared_statement.h>
 
 #ifndef ARTDBCOMMUNICATOR_H
 #define ARTDBCOMMUNICATOR_H
@@ -14,8 +19,6 @@ class ArtDBCommunicator{
     const string DBname;
     const string address;
     const string DBUsername;
-
-
 };
 
 #endif ARTDBCOMMUNICATOR_H
