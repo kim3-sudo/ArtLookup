33145980da16072d0f04d9d2940afd417bd50a70
Ryan Schultz <ryanschultz2018@yahoo.com>
2019-11-12 15:42:29 -0500

    Created constructors. Created function to increment the number of likes

--- artlookup/Artwork.cpp
+++ artlookup/Artwork.cpp
@@ -1,13 +1,32 @@
+#include "Artwork.h"
 
+Artwork::Artwork()
+{
 
+}
 
+/*Preconditions: All variables must come from the art table in the Team5 sql
+  database. The parameters are listed in the same order as the respective
+  columns
+  Postconditions: Relevant data is stored in member variables including ArtistInfo, ArtSetting and ArtStyle structs
+*/
+//This is very long MICHAELA!!! (Not mad just need your help)
+Artwork::Artwork(int artIdFromDB, string titleFromDB, string urlFromDB, int numLikesFromDB, string authorFromDB, string birthDeathFromDB, int dateFromDB, string locationFromDB, string timeframeFromDB, string techniqueFromDB, string schoolFromDB, string typeFromDB, string formFromDB)
+{
+  artId = artIdFromDB;  //Alternate solution this->artId = artId
+  title = titleFromDB;
+  url = urlFromDB;
+  numLikes = numLikesFromDB;
 
+  ArtistInfo artistInfo{ authorFromDB, birthDeathFromDB };
+  ArtSetting setting{ dateFromDB, locationFromDB, timeframeFromDB };
+  ArtStyle style{ techniqueFromDB, schoolFromDB, typeFromDB, formFromDB };
+}
 
-#include "mysql_connection.h"
-#include "mysql_driver.h"
-#include <cppconn/driver.h>
-#include <cppconn/exception.h>
-#include <cppconn/resultset.h>
-#include <cppconn/statement.h>
-#include <cppconn/prepared_statement.h>
-#include "ArtLookup.h"
+/*Preconditions: None
+  Postconditions: The member variable numLikes will be incremented
+*/
+void Artwork::incrementLikes()
+{
+  numLikes++;
+}
--- artlookup/Artwork.h
+++ artlookup/Artwork.h
@@ -1,5 +1,13 @@
-#using <iostream>
+//#using <iostream>
 #using <string>
+
+#include "mysql_connection.h"
+#include "mysql_driver.h"
+#include <cppconn/driver.h>
+#include <cppconn/exception.h>
+#include <cppconn/resultset.h>
+#include <cppconn/statement.h>
+#include <cppconn/prepared_statement.h>
 
 using namespace std;
 
