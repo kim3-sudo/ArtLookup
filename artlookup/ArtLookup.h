e19c1a9a454b317b89d3c2c5f3f451c6ec9fd28e
Michaela Brydon <brydon1@kenyon.edu>
2019-11-13 17:05:37 -0500

    Edited ArtLookup.h; made proper inherited class

--- artlookup/ArtLookup.h
+++ artlookup/ArtLookup.h
@@ -7,13 +7,15 @@
 // using std::auto_ptr;
 
 #include <string>
-#include "Artwork"
 #include <vector>
+#include "ArtDBCommunicator.h"
+#include "Artwork.h"
+
 
 #ifndef ART_LOOKUP_H
 #define ART_LOOKUP_H
 
-class ArtLookup{
+class ArtLookup: public ArtDBCommunicator{
   public:
   	vector<Artwork> lookupAll(string search);
   	vector<Artwork> lookupSingle(string search, string colName);
