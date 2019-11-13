ed21dcd4cfd8c899a9f7bea3c08fa81f7261a6c8
Michaela Brydon <brydon1@kenyon.edu>
2019-11-13 17:06:16 -0500

    Started implementation of ArtLookup.cpp

--- artlookup/Artwork.cpp
+++ artlookup/Artwork.cpp
@@ -1,35 +1,20 @@
-//File Name: Artwork.cpp
-//Author: Ryan, Michaela, and Sejin
-//Email Address: schultz4@kenyon.edu, kim3@kenyon.edu, brydon1@kenyon.edu
-//Assignment: Project 3
-//Description: Data representation of a piece of art
-//Last Changed: November 12, 2019
-
 #include "Artwork.h"
 
-//Default constructor artId is set to -1 all other variables are set to
-//appropriate 'empty' values
-Artwork::Artwork(){
-  artId = -1;
-  title = "";
-  url = ""
-  numLikes = 0;
+Artwork::Artwork()
+{
 
-  ArtistInfo artistInfo{ "", "" };
-  ArtSetting setting{ 0, "", "" };
-  ArtStyle style{ "", "", "", "" };
 }
 
-/*
-  Preconditions: All variables must come from the art table in the Team5 sql
+/*Preconditions: All variables must come from the art table in the Team5 sql
   database. The parameters are listed in the same order as the respective
   columns
   Postconditions: Relevant data is stored in member variables including ArtistInfo, ArtSetting and ArtStyle structs
 */
 //This is very long MICHAELA!!! (Not mad just need your help)
-Artwork::Artwork(int artIdFromDB, string titleFromDB, string urlFromDB,
-  int numLikesFromDB, string authorFromDB, string birthDeathFromDB, int dateFromDB,
-  string locationFromDB, string timeframeFromDB, string techniqueFromDB, string schoolFromDB, string typeFromDB, string formFromDB)
+Artwork::Artwork(int artIdFromDB, string titleFromDB, string urlFromDB, 
+  int numLikesFromDB, string authorFromDB, string birthDeathFromDB, int dateFromDB, 
+  string locationFromDB, string timeframeFromDB, string techniqueFromDB, string schoolFromDB,
+   string typeFromDB, string formFromDB)
 {
   artId = artIdFromDB;  //Alternate solution this->artId = artId
   title = titleFromDB;
@@ -49,10 +34,10 @@ Artwork::Artwork(int artIdFromDB, string titleFromDB, string urlFromDB,
   artStyle.form = formFromDB;
 }
 
-/*
-  Preconditions: None
+/*Preconditions: None
   Postconditions: The member variable numLikes will be incremented
 */
-void Artwork::incrementLikes(){
+void Artwork::incrementLikes()
+{
   numLikes++;
 }
