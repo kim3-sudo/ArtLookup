8198a3df577dec8ab961c1eb43eb6bf86988e946
Michaela Brydon <brydon1@kenyon.edu>
2019-11-15 15:28:21 -0500

    Changed auto_ptr to unique_ptr

--- artlookup/ArtDBCommunicator.cpp
+++ artlookup/ArtDBCommunicator.cpp
@@ -13,10 +13,12 @@ ArtDBCommunicator::ArtDBCommunicator(){
 }
 
 // Changed name from establishConnection
-std::auto_ptr<sql::Connection> ArtDBCommunicator::establishDBConnection(){
+//std::auto_ptr<sql::Connection> ArtDBCommunicator::establishDBConnection(){
+std::unique_ptr<sql::Connection> ArtDBCommunicator::establishDBConnection(){
 
   sql::Driver* driver = sql::mysql::get_driver_instance();
-  std::auto_ptr<sql::Connection> con(driver->connect(url, user, pass));
+  //std::auto_ptr<sql::Connection> con(driver->connect(url, user, pass));
+  std::unique_ptr<sql::Connection> con(driver->connect(url, user, pass));
   con->setSchema(database);
 
   return con;
--- artlookup/ArtDBCommunicator.h
+++ artlookup/ArtDBCommunicator.h
@@ -37,7 +37,8 @@ class ArtDBCommunicator{
     ArtDBCommunicator();
 
     // ERROR: template<class> class std::auto_ptr is deprecated 
-    std::auto_ptr<sql::Connection> establishConnection();
+    //std::auto_ptr<sql::Connection> establishConnection();
+    std::unique_ptr<sql::Connection> establishConnection();
   private:
     const string password = PASS;
     const string DBName = DB;
