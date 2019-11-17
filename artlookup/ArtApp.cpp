521d8762096aa36dc86b0ab978b8c6bb02f04e32
Ryan Schultz <ryanschultz2018@yahoo.com>
2019-11-17 17:48:05 -0500

    Added comments.

--- artlookup/ArtApp.cpp
+++ artlookup/ArtApp.cpp
@@ -16,6 +16,8 @@ using std::cout;
 
 class JSCommunicator(){
   string print(Artwork artwork){
+    //Prints artwork data to JavaScript delimited by *^. It is possible to do
+    //multi-character delimination in JavaScript.
     string result, sep("*^");
     result += artwork.getArtId() + sep + artwork.getArtistInfo().author + sep + artwork.getArtistInfo().birthDeath + sep + artwork.getTitle() + sep + artwork.getArtStyle().technique + sep + artwork.getArtSetting().location + sep + artwork.getUrl() + sep + artwork.getArtStyle().form + sep + artwork.getArtStyle().type + sep + artwork.getArtStyle().school + sep + artwork.getArtSetting().timeframe + sep + artwork.getArtSetting().date + sep + artwork.getNumLikes()
     return result;
@@ -28,13 +30,19 @@ int main(){
   // Ajax object to receive info from web page
   form_iterator itSearch = cgi.getElement("searchVal");
   string searchVal = **itSearch;
+
   Artlookup artlookup;
   Query query;
+
+  //Vector of artworks. Created using matchSingleCol query for our lookupSingleCommand function.
   vector<Artwork> searchByTitle(artlookup.lookupSingleCommand(query.matchSingleCol(searchVal,"Title")));
+
   string colNames[13]= {"artId","Author","Born-Diec","Title","Technique","Location","URL","Form", "Type", "School", "Timeframe", "Date", "Likes"},result("Content-Type: text/plain\n\n");
 
   Artwork artwork;
   JSCommunicator jSCommunicator;
+
+  //Sends artwork data to JavaScript
   for (uint i=0;i<searchByTitle.size()){
     artwork = searchByTitle.at(i);
     result += jSCommunicator.print(artwork);
--- html/ArtApp.js
+++ html/ArtApp.js
@@ -1,10 +1,12 @@
 
 
 $(document).ready(function () {
+  //getMatches when search button is clicked
   $(".action-button").click(getMatches);
-  //search-field
 });
 
+//Empties photo gallery and then fills with new images from our c++ file
+//through the function processResults
 function getMatches(){
     $('.photo-gallery').empty();
     $.ajax({
@@ -15,21 +17,26 @@ function getMatches(){
     });
 }
 
+//Empties photo gallery (again?) builds new gallery using buildGallery function
 function processResults(results) {
     console.log("Results:"+results);
     $('.photo-gallery').empty();
     $('.photo-gallery').append(buildGallery(results));
 }
 
+//Parses art data from c++. Appends all photos to photo gallery
 function buildGallery(list){
   var artData = list.split("*^");
 
+  //If there is nothing from c++ return "Internal Error"
   if (artData.length < 1) {
 	  return "<h3>Internal Error</h3>";
   } else {
 
+    //Creates container in lightbox
     var result = '<div class="container">';
 
+    //Creates photos using the url from c++
     var listLength = artData.length;
     for (var i = 6; i < listLength; i+=13) {
       <div class="col-sm-6 col-md-4 col-lg-3 item"><a data-lightbox="photos" href=artData[i]><img class="img-fluid" src=artData[i]></a></div>
