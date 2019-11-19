6b40ea7094772cdcd87eaa3d1b52963c579af4e4
Ryan Schultz <50674503+5chu17sRyan@users.noreply.github.com>
2019-11-19 16:09:40 -0500

    Commented out old console logs

--- html/ArtApp.js
+++ html/ArtApp.js
@@ -33,7 +33,7 @@ function getMatches(){
 
 //Empties photo gallery (again?) builds new gallery using buildGallery function
 function processResults(results) {
-    console.log("Results:"+results);
+    //console.log("Results:"+results);
     $('.photo-gallery').empty();
     $('.photo-gallery').append(buildGallery(results));
 }
@@ -51,13 +51,13 @@ function buildGallery(list){
       var result = '<div class="container">';
 
       var listLength = artData.length;
-      console.log("Length of artData List: ");
-      console.log(listLength);
-      console.log("Appending Results: \n\n");
+      //console.log("Length of artData List: ");
+      //console.log(listLength);
+      //console.log("Appending Results: \n\n");
       for (var i = 7; i < listLength; i+=13){
-    	  console.log(i);
-    	  console.log("ArtData[i]: ");
-    	  console.log(artData[i]);
+    	  //console.log(i);
+    	  //console.log("ArtData[i]: ");
+    	  //console.log(artData[i]);
     	  result += '<div class="col-sm-6 col-md-4 col-lg-3 item"><a data-lightbox="photos" href=' + artData[i] + '><img class="img-fluid" src=' + artData[i] + '></a></div>'
       }
     result += "</div>";
