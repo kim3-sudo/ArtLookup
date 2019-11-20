fb138b94e47b93ff47b1440001209ceeeb5cac90
Ryan Schultz <ryanschultz2018@yahoo.com>
2019-11-20 15:59:26 -0500

    Modified variables in class.

--- artlookup/Member.h
+++ artlookup/Member.h
@@ -5,22 +5,20 @@ using namespace std;
 #ifndef MEMBER_H
 #define MEMBER_H
 
-//I'm not sure if I like using a struct here - Ryan
-struct MemberInfo{
-  string m_email;
-  string m_password;
-  string m_username;
-};
-
 class Member{
 public:
   Member();
-  Member(string email, string password, string username);
-  void like(int objectId, string objectType);
-  void comment(int objectId, string objectType, string comment);
-  bool isValidMember();
+  Member(string memberId, string username, string password, string email);
+
+  void likeArtwork(Artwork artwork);
+  //void likeComment(Comment comment);
+  void commentOnArtwork(string comment, Artwork artwork);
+  //void replyToComment(string reply, Comment comment);
 private:
-  MemberInfo info;
+  int m_memberId;
+  string m_username;
+  string m_password;
+  string m_email;
 };
 
 #endif
