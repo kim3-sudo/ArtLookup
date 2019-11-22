0a6e9f369e0cf6b2128215aa271ae4c703958642
Michaela Brydon <brydon1@kenyon.edu>
2019-11-21 20:02:37 -0500

    Maybe now the background will be white

--- html/ArtApp.js
+++ html/ArtApp.js
@@ -19,7 +19,7 @@ function getMatches(){
     $('#home').hide();
     console.log("hide the homepage");
     $('#search').show();
-    document.body.style.background = rgb(255,255,255);
+    document.body.style.background = 'white';
     //$('#'+$(this).attr('search')).show();
     console.log("Show seach page");
     //what this *should* do: hide the home, display the search page
