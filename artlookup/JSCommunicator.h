f5a5842be4672d129587fda35acf8d10929cbca4
Ryan Schultz <ryanschultz2018@yahoo.com>
2019-12-03 15:59:21 -0500

    Added header file for JSCommunicator.

---
+++ artlookup/JSCommunicator.h
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <string>
+
+#include "Artwork.h"
+
+#include "cgicc/Cgicc.h"
+#include "cgicc/HTTPHTMLHeader.h"
+#include "cgicc/HTMLClasses.h"
+
+#ifndef JSCOMMUNICATOR_H
+#define JSCOMMUNICATOR_H
+
+class JSCommunicator{
+  public:
+    string print(Artwork artwork);
+    string getElement(string element, Cgicc &cgi);
+};
+
+#endif //JSCOMMUNICATOR_H
