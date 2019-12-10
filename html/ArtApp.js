f2f5fd9b4e0591b4516e1cf763dad0840a011a70
Michaela Brydon <brydon1@kenyon.edu>
2019-12-09 20:26:36 -0500

    Tweaks to class ids in html

--- html/ArtApp.js
+++ html/ArtApp.js
@@ -1,8 +1,6 @@
 var searchCategory;  //Category to be searched by: Title, Author ...
 var ajaxUser = "brydon1"; //Your username for ajax calls
 
-// Small change
-
 $(document).ready(function () {
   console.log("ready!");
   checkCookie();
@@ -73,13 +71,13 @@ function showPhotos(list){
         console.log(artData[i]);
 
         //Creates image
-        result += '<img class="img-fluid lazyload" src=' + artData[i] + ' height="100%">';
+        result += '<img class="img-fluid lazyload" src=' + artData[i] + ' height="100%" align="left">';
         //Creates description
         result += '<p style="padding-top: 8px;">Title: ' + artData[i-3] + '<br><br>Author: ' + artData[i-5] + ' ' + artData[i-4] + '<br>Location: ' + artData[i-1] + '<br>Date: ' + artData[i+5] + '<br>Technique: ' + artData[i-2] + '<br>School: ' + artData[i+3] + '<br>Type: ' + artData[i+2] + '<br>Form: ' + artData[i+1] + '</p>';
         //Creates like button
-        result += '<button class="btn btn-warning text-center" type="button" style="margin-top: 0px;margin-bottom: 10px;">like!</button>';
+        result += '<button class="btn btn-warning text-center likeClass" type="button" style="margin-top: 0px;margin-bottom: 10px;">like!</button>';
         //Creates comment field and submit button
-        result += '<form><div class="form-group"><input class="form-control" type="text" placeholder="comment here!"><button class="btn btn-light" type="button" style="margin-bottom: 70px;margin-top: 10px;">submit</button>';
+        result += '<form><div class="form-group"><input class="form-control commentClass" type="text" placeholder="comment here!"><button class="btn btn-light" type="button" style="margin-bottom: 70px;margin-top: 10px;">submit</button>';
         //Adds closing tags
         result += '</div></form></div></div></div>';
       }
