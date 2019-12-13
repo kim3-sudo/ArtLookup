b00291646c729f23d689c8ac91ed513e9c1a391a
Michaela Brydon <brydon1@kenyon.edu>
2019-12-12 19:48:35 -0500

    Changed test.js

--- html/test.js
+++ html/test.js
@@ -1,13 +1,14 @@
 $(document).ready(function () {
-  document.cookie = "memberId=4;username=sammy";
+  //document.cookie = "memberId=4;username=sammy";
   // document.cookie = "Michaela=gone";
   // document.cookie = "John=done";
   // document.cookie = "Michaela=crazy";
-  var decodedCookie = decodeURIComponent(document.cookie);
-  var parsed = decodedCookie.split(';');
-  console.log("Number of Cookies: ", parsed.length);
-  for (var i=0; i<parsed.length; i++) {
-    console.log(parsed[i]);
-  }
+  //var decodedCookie = decodeURIComponent(document.cookie);
+  var emptyString = "";
+  var parsed = emptyString.split(';');
+  console.log("Split Empty String Length: ", parsed.length);
+  // for (var i=0; i<parsed.length; i++) {
+  //   console.log(parsed[i]);
+  // }
   console.log("Done");
 });
