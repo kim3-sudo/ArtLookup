4dc97d348d0eb725f9e965681726a5f49773f38f
Ryan Schultz <ryanschultz2018@yahoo.com>
2019-12-19 16:59:51 -0500

    Commented out query to update art table.

--- artlookup/LikePhoto.cpp
+++ artlookup/LikePhoto.cpp
@@ -36,7 +36,7 @@ int main(){
   string updatedLikes = to_string(numLikes);
 
   // Updates number of likes in art table in database
-  string updateLikesQuery = query.updateNumLikes(updatedLikes, artId);
+  //string updateLikesQuery = query.updateNumLikes(updatedLikes, artId);
 
   //artlookup.updateTable(updateLikesQuery);
 
